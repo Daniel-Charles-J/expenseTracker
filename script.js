f625b9b49b051d65f4d58d045462f1a89459ebbd
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2022-05-14 16:23:22 +0530

    data got through id's in javaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+const balance = document.getElementById('balance');
+const moneyPlus = document.getElementById('money-plus');
+const moneyMinus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const transaction = document.getElementById('transaction');
+const amount = document.getElementById('amount');
